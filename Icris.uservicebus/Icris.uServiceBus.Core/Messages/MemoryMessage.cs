9b54ceabcb8a84a3defb94883b039a21beb6c1e3
Andre Boonzaaijer <andre.boonzaaijer@icris.nl>
2017-09-08 14:50:53 +0200

    starting in-memory implementations

---
+++ Icris.uservicebus/Icris.uServiceBus.Core/Messages/MemoryMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icris.uServiceBus.Core.Messages
+{
+    public class MemoryMessage<T> : IMessage<T>
+    {
+        T value;
+        public MemoryMessage(T value)
+        {
+            this.value = value;
+        }
+        public void Complete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Content()
+        {
+            return value;
+        }
+
+        public void Release()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenewLock()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Icris.uservicebus/Icris.uServiceBus.Core/Queues/MemoryQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Icris.uServiceBus.Core.Messages;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Icris.uServiceBus.Core.Queues
+{
+    public class MemoryQueue<T> : IQueue<T>
+    {
+        ConcurrentBag<IMessage<T>> messages = new ConcurrentBag<IMessage<T>>();
+        ConcurrentBag<Tuple<DateTime, IMessage<T>>> lockedmessages = new ConcurrentBag<Tuple<DateTime, IMessage<T>>>();
+
+        public void Clear()
+        {
+            lockedmessages = new ConcurrentBag<Tuple<DateTime, IMessage<T>>>();
+            messages = new ConcurrentBag<IMessage<T>>();
+        }
+
+        public IMessage<T> Receive()
+        {
+            IMessage<T> value;
+            while (!messages.TryTake(out value))
+            {
+                Thread.Sleep(1);
+                return Receive();
+            }
+            lockedmessages.Add(new Tuple<DateTime, IMessage<T>>(DateTime.Now,value));
+            return value;
+        }
+
+        public void Send(T payload)
+        {
+            messages.Add(new MemoryMessage<T>(payload));
+        }
+
+        public void Subscribe(Action<IMessage<T>> notification)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
